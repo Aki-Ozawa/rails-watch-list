49816023589c4119f8a1cd9394410dfd30107e2d
Aki Ozawa <ozazachann@gmail.com>
2023-08-18 09:37:21 +0100

    half way

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+
+
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['API_KEY_TMDB']}"
+
+
+
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+connect = URI.open(url).read
+parsed_json  = JSON.parse(connect)
+
+puts "Getting ze mouvies with my french accent"
+base_poster_url = "https://image.tmdb.org/t/p/original"
+parsed_json["results"].each do |movie|
+    Movie.create({title: movie["title"], overview: movie["overview"] , poster_url: "#{base_poster_url}#{movie['poster_path']}"})
+end
+
+puts "Hai ! Owari da :D"
